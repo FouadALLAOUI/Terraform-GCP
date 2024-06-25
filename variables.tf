9804d67e1eb810c946fdae652f23d30a1fe052b3
FouadALLAOUI <102102438+FouadALLAOUI@users.noreply.github.com>
2024-06-25 15:52:28 +0100

    start vars

---
+++ variables.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "terraform-427513"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
+
+resource "google_compute_network" "app" {
+  name                    = "app"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "app" {
+  name          = "app"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.app.id
+}
+
+
+data "google_compute_image" "ubuntu" {
+  most_recent = true
+  project     = "ubuntu-os-cloud" 
+  family      = "ubuntu-2204-lts"
+}
+
+resource "google_compute_instance" "web" {
+  name         = "web"
+  machine_type = "e2-micro"
+
+  
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu.self_link
+    }
+  }
+  network_interface {
+   subnetwork = "app"
+   access_config {
+      # Leave empty for dynamic public IP
+    }
+  }  
+
+  allow_stopping_for_update = true
+
+}
